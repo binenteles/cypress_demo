c2b68197e753ca16df3848685969ae7fe772d374
AlexJelescu <ajelescu@caesars.com>
2025-03-24 21:26:36 +0200

    changes to avoid exit 1 for missing reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "cypress"
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   reporter: 'cypress-multi-reporters',
@@ -9,6 +9,13 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
 
+    },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: false,
+      json: true,
     },
     specPattern: 'cypress/e2e/**/*.spec.ts',
     baseUrl: 'https://www.saucedemo.com/',
